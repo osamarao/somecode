63f2c14d98bd2354a584659f17a4b03fb7a5580e
Osama Rao <a653h496@Ennovar-MarCom5.local>
2016-05-03 17:06:19 -0500

    Protocols Activity

--- app/src/main/java/io/clutchstud/nfems/activities/ContentActivity.java
+++ app/src/main/java/io/clutchstud/nfems/activities/ContentActivity.java
@@ -1,6 +1,7 @@
 package io.clutchstud.nfems.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -14,6 +15,10 @@ public class ContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_content);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (getIntent().hasExtra("catId")) {
+            Snackbar.make(this.findViewById(R.id.toolbar), "catId is : " + getIntent().getIntExtra("catId", 0), Snackbar.LENGTH_SHORT).show();
+        }
 
 
     }
--- app/src/main/java/io/clutchstud/nfems/activities/MainActivity.java
+++ app/src/main/java/io/clutchstud/nfems/activities/MainActivity.java
@@ -1,37 +1,23 @@
 package io.clutchstud.nfems.activities;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SubMenu;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
-import java.util.Arrays;
-
 import io.clutchstud.nfems.R;
-import io.clutchstud.nfems.models.CategoryRealmObject;
 import io.clutchstud.nfems.services.SyncProtocolsService;
-import io.clutchstud.nfems.util.CategoryTitleMenuItem;
-import io.clutchstud.nfems.util.MenuPopulator;
-import io.clutchstud.nfems.util.NavigationSubMenuPopulator;
-import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, PopulatesMenu {
 
@@ -45,8 +31,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        EventBus.getDefault().register(this);
-        //gainPermission();
+       // EventBus.getDefault().register(this);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -59,53 +44,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
         setPopulateMenuStrategy(new PopulateMenuFromDatabaseStrategy());
-
         populateMenuStrategy.populateMenu(this, navigationView);
-
-
     }
 
-    private void gainPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EventBus.getDefault().register(this);
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 90);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-
-            realmConfiguration = new RealmConfiguration.Builder(this).build();
-        }
     }
-
 
     @Subscribe
     public void handleSyncDoneEvent(SyncProtocolsService.SyncDoneEvent event){
         // We can now update the navigation
         Snackbar.make(this.findViewById(R.id.toolbar), "Sync Complete", Snackbar.LENGTH_SHORT).show();
-        Realm.setDefaultConfiguration(new RealmConfiguration.Builder(this).build());
-        RealmResults<CategoryRealmObject> categories = Realm.getDefaultInstance().where(CategoryRealmObject.class).findAll();
-        Log.d("handleSomething", Arrays.deepToString(categories.toArray()));
+//        Realm.setDefaultConfiguration(new RealmConfiguration.Builder(this).build());
+//        RealmResults<CategoryRealmObject> categories = Realm.getDefaultInstance().where(CategoryRealmObject.class).findAll();
+//        Log.d("handleSomething", Arrays.deepToString(categories.toArray()));
+//
+//        Menu m = navigationView.getMenu();
+//        SubMenu topChannelMenu = m.addSubMenu("Categories");
+//
+//        MenuPopulator menuPopulator = new NavigationSubMenuPopulator(topChannelMenu);
+//        for (CategoryRealmObject category : categories){
+//            menuPopulator.add(new CategoryTitleMenuItem(Menu.NONE, category.getId(), Menu.NONE, category.getCategory_name()));
+//        }
 
-        Menu m = navigationView.getMenu();
-        SubMenu topChannelMenu = m.addSubMenu("Categories");
-
-        MenuPopulator menuPopulator = new NavigationSubMenuPopulator(topChannelMenu);
-        for (CategoryRealmObject category : categories){
-            menuPopulator.add(new CategoryTitleMenuItem(Menu.NONE, category.getId(), Menu.NONE, category.getCategory_name()));
-        }
+        populateMenuStrategy.populateMenu(this, navigationView);
     }
 
     @Override
@@ -153,22 +118,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        Intent contentActivityIntent = new Intent(this, ContentActivity.class);
+        contentActivityIntent.putExtra("catId", id);
+
+        startActivity(contentActivityIntent);
+
         return true;
     }
 
